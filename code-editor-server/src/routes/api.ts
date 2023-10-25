453bfe1de4bc68a2728f755778c4f8ff3543749e
Fred Durham <f.durham@protonmail.com>
2023-10-25 13:10:42 -0500

    updating with David changes

---
+++ code-editor-server/src/routes/api.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+const { getOrCreateDoc } = require("@y-sweet/sdk");
+import { CONNECTION_STRING } from "../utils/constants";
+
+const router = express.Router();
+
+router.get("/get-token/:docId", async (req, res) => {
+  let docId: string | undefined = req.params.docId;
+  if (docId === "default") {
+    docId = undefined;
+  }
+  const clientToken = await getOrCreateDoc(docId, CONNECTION_STRING);
+  res.json({ clientToken });
+});
+
+export default router;
--- code-editor-server/src/server.ts
+++ code-editor-server/src/server.ts
@@ -1,36 +1,21 @@
 import express, { RequestHandler, Request, Response } from "express";
-const { getOrCreateDoc } = require("@y-sweet/sdk");
 import cors from "cors";
-// import codeRouter from './routes/routes';
 import path from "path";
+import apiRouter from "./routes/api";
 
 const app = express();
 const port = 3001;
-const CONNECTION_STRING =
-  "yss://y-sweet-server-worker-staging.davidrd123.workers.dev";
 
 app.use(cors() as RequestHandler);
 app.use(express.json());
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../build")));
-}
-// app.use('/api/codeEval', codeRouter);
+
+app.use(express.static(path.join(__dirname, "../build")));
+app.use("/api", apiRouter);
+
 console.log(path.join(__dirname, "../build"));
 
 app.get("/hello", (req, res) => {
   res.send("Hello World From Editor Server!");
-});
-
-// temporarily adding '/api/' in front of '/get-token' to make local development work properly
-// removing before merging changes into main until can talk to David and understand the recent change
-
-app.get("/api/get-token/:docId", async (req, res) => {
-  let docId: string | undefined = req.params.docId;
-  if (docId === "default") {
-    docId = undefined;
-  }
-  const clientToken = await getOrCreateDoc(docId, CONNECTION_STRING);
-  res.json({ clientToken });
 });
 
 app.listen(port, () => {
