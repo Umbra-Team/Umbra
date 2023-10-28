790db2c2419c84c7d0eedb7195a7812202f2515b
Fred Durham <f.durham@protonmail.com>
2023-10-28 17:49:13 -0500

    changed Card style
    Co-authored-by: Bethany Pietroniro <bethany.pietroniro28@gmail.com>
    Co-authored-by: Michael Johnson <elsif.maj@gmail.com>
    Co-authored-by: David Dickinson <davidrd123@gmail.com>

--- code-editor-client/src/components/LibraryDrawer.tsx
+++ code-editor-client/src/components/LibraryDrawer.tsx
@@ -15,6 +15,7 @@ import {
   Button,
   Text,
   Code,
+  Flex,
 } from "@chakra-ui/react";
 
 const LibraryDrawer = ({ placement, onClose, isOpen, size }) => {
@@ -33,120 +34,258 @@ const LibraryDrawer = ({ placement, onClose, isOpen, size }) => {
         <DrawerBody bgGradient='linear(to-r, black, gray.100, blue.800)'>
           <SimpleGrid
             spacing={5}
-            templateColumns='repeat(auto-fill, minmax(200px, 1fr))'
+            templateColumns='repeat(auto-fill, minmax(300px, 1fr))'
           >
-            <Card bgColor='whiteAlpha.900'>
-              <CardHeader>
+            <Card bgColor='gray.800' pl='2' pr='2' minH='300px'>
+              <CardHeader textAlign='center'>
                 <Heading size='md' color='gray.100'>
                   Hello World
                 </Heading>
               </CardHeader>
-              <CardBody border='2px' borderRadius='10' borderColor='black'>
-                <Code>"Some code"</Code>
+              <CardBody
+                bg='#232D3F'
+                border='2px'
+                borderRadius='10'
+                borderColor='black'
+                color='white'
+              >
+                "Some Code"
               </CardBody>
-              <CardFooter>
-                <Button
-                  color='whitek'
-                  bgColor='blue.700'
-                  _hover={{ bg: "blue.900" }}
-                >
-                  Insert Into Editor
-                </Button>
+              <CardFooter p={2}>
+                <Flex gap='5px' justifyContent='space-between' pr='3' pl='4'>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Insert Into Editor
+                  </Button>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Delete Snippet
+                  </Button>
+                </Flex>
               </CardFooter>
             </Card>
-            <Card bgColor='whiteAlpha.900'>
-              <CardHeader>
+            <Card bgColor='gray.800' pl='2' pr='2' minH='300px'>
+              <CardHeader textAlign='center'>
                 <Heading size='md' color='gray.100'>
-                  FizzBuzz
+                  Hello World
                 </Heading>
               </CardHeader>
-              <CardBody border='2px' borderRadius='10' borderColor='black'>
-                <Code>"Some code"</Code>
+              <CardBody
+                bg='#232D3F'
+                border='2px'
+                borderRadius='10'
+                borderColor='black'
+                color='white'
+              >
+                "Some Code"
               </CardBody>
-              <CardFooter>
-                <Button
-                  color='whitek'
-                  bgColor='blue.700'
-                  _hover={{ bg: "blue.900" }}
-                >
-                  Insert Into Editor
-                </Button>
+              <CardFooter p={2}>
+                <Flex gap='5px' justifyContent='space-between' pr='3' pl='4'>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Insert Into Editor
+                  </Button>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Delete Snippet
+                  </Button>
+                </Flex>
               </CardFooter>
             </Card>
-            <Card bgColor='whiteAlpha.900'>
-              <CardHeader>
+            <Card bgColor='gray.800' pl='2' pr='2' minH='300px'>
+              <CardHeader textAlign='center'>
                 <Heading size='md' color='gray.100'>
-                  Spiral Matrix
+                  Hello World
                 </Heading>
               </CardHeader>
-              <CardBody border='2px' borderRadius='10' borderColor='black'>
-                <Code>"Some code"</Code>
+              <CardBody
+                bg='#232D3F'
+                border='2px'
+                borderRadius='10'
+                borderColor='black'
+                color='white'
+              >
+                "Some Code"
               </CardBody>
-              <CardFooter>
-                <Button
-                  color='whitek'
-                  bgColor='blue.700'
-                  _hover={{ bg: "blue.900" }}
-                >
-                  Insert Into Editor
-                </Button>
+              <CardFooter p={2}>
+                <Flex gap='5px' justifyContent='space-between' pr='3' pl='4'>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Insert Into Editor
+                  </Button>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Delete Snippet
+                  </Button>
+                </Flex>
               </CardFooter>
             </Card>
-            <Card bgColor='whiteAlpha.900'>
-              <CardHeader>
+            <Card bgColor='gray.800' pl='2' pr='2' minH='300px'>
+              <CardHeader textAlign='center'>
                 <Heading size='md' color='gray.100'>
-                  Fibonaci
+                  Hello World
                 </Heading>
               </CardHeader>
-              <CardBody border='2px' borderRadius='10' borderColor='black'>
-                <Code>"Some code"</Code>
+              <CardBody
+                bg='#232D3F'
+                border='2px'
+                borderRadius='10'
+                borderColor='black'
+                color='white'
+              >
+                "Some Code"
               </CardBody>
-              <CardFooter>
-                <Button
-                  color='whitek'
-                  bgColor='blue.700'
-                  _hover={{ bg: "blue.900" }}
-                >
-                  Insert Into Editor
-                </Button>
+              <CardFooter p={2}>
+                <Flex gap='5px' justifyContent='space-between' pr='3' pl='4'>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Insert Into Editor
+                  </Button>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Delete Snippet
+                  </Button>
+                </Flex>
               </CardFooter>
             </Card>
-            <Card bgColor='whiteAlpha.900'>
-              <CardHeader>
+            <Card bgColor='gray.800' pl='2' pr='2' minH='300px'>
+              <CardHeader textAlign='center'>
                 <Heading size='md' color='gray.100'>
-                  HTML 5 Skeleton
+                  Hello World
                 </Heading>
               </CardHeader>
-              <CardBody border='2px' borderRadius='10' borderColor='black'>
-                <Code>"Some code"</Code>
+              <CardBody
+                bg='#232D3F'
+                border='2px'
+                borderRadius='10'
+                borderColor='black'
+                color='white'
+              >
+                "Some Code"
               </CardBody>
-              <CardFooter>
-                <Button
-                  color='whitek'
-                  bgColor='blue.700'
-                  _hover={{ bg: "blue.900" }}
-                >
-                  Insert Into Editor
-                </Button>
+              <CardFooter p={2}>
+                <Flex gap='5px' justifyContent='space-between' pr='3' pl='4'>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Insert Into Editor
+                  </Button>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Delete Snippet
+                  </Button>
+                </Flex>
               </CardFooter>
             </Card>
-            <Card bgColor='whiteAlpha.900'>
-              <CardHeader>
+            <Card bgColor='gray.800' pl='2' pr='2' minH='300px'>
+              <CardHeader textAlign='center'>
                 <Heading size='md' color='gray.100'>
-                  Even more code
+                  Hello World
                 </Heading>
               </CardHeader>
-              <CardBody border='2px' borderRadius='10' borderColor='black'>
-                <Code>"Some code"</Code>
+              <CardBody
+                bg='#232D3F'
+                border='2px'
+                borderRadius='10'
+                borderColor='black'
+                color='white'
+              >
+                "Some Code"
               </CardBody>
-              <CardFooter>
-                <Button
-                  color='whitek'
-                  bgColor='blue.700'
-                  _hover={{ bg: "blue.900" }}
-                >
-                  Insert Into Editor
-                </Button>
+              <CardFooter p={2}>
+                <Flex gap='5px' justifyContent='space-between' pr='3' pl='4'>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Insert Into Editor
+                  </Button>
+                  <Button
+                    borderRadius='15'
+                    color='white'
+                    whiteSpace='normal'
+                    overflow='hidden'
+                    w='49%'
+                    bgColor='blue.700'
+                    _hover={{ bg: "blue.900" }}
+                  >
+                    Delete Snippet
+                  </Button>
+                </Flex>
               </CardFooter>
             </Card>
           </SimpleGrid>
