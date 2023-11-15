d96e1d0f0e94aeb774b268112fbe0e9be4a2f57c
Fred Durham <f.durham@protonmail.com>
2023-11-15 09:42:50 -0600

    changed CONNECTION_STRING for our cloudflare worker to our shared cloudflare account instead of David's
    Co-authored-by: Fred Durham <fdurham@protonmail.com>
    Co-authored-by: Bethany Pietroniro <bethany.pietroniro28@gmail.com>
    Co-authored-by: Michael Johnson <elsif.maj@gmail.com>
    Co-authored-by: David Dickinson <davidrd123@gmail.com>

--- code-editor-server/src/server.ts
+++ code-editor-server/src/server.ts
@@ -1,41 +1,38 @@
 import express, { RequestHandler, Request, Response } from "express";
-import './utils/awsConfig';
+import "./utils/awsConfig";
 import cors from "cors";
 import path from "path";
 import apiRouter from "./routes/api";
 import { errorHandler } from "./utils/middleware";
 import sequelize from "./utils/sequelize";
-import './models/associations'
+import "./models/associations";
 import { syncUsers } from "./scripts/syncUsers";
 import { wipeUsers, wipeSnippets } from "./scripts/deleteDB";
-import morgan from "morgan"; 
+import morgan from "morgan";
 
 const app = express();
 const port = 3001;
 
-const CODE_EXECUTION_ENDPOINT = 'http://35.81.242.17:2000/api/v2/execute';
-
 // Test and sync database connections
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('Connection to PostgreSQL has been established successfully.');
-    return sequelize.sync({ alter: true });  // Update tables if needed
+    console.log("Connection to PostgreSQL has been established successfully.");
+    return sequelize.sync({ alter: true }); // Update tables if needed
   })
   .then(() => {
-    
     // wipeUsers();  // Wipe users
     // wipeSnippets();  // Wipe snippets
-    return syncUsers();  // Sync users from Cognito
-    console.log('All PostgreSQL tables have been successfully created.');
+    return syncUsers(); // Sync users from Cognito
+    console.log("All PostgreSQL tables have been successfully created.");
   })
   .catch((error) => {
-    console.error('Unable to connect to PostgreSQL:', error);
+    console.error("Unable to connect to PostgreSQL:", error);
   });
-
 
 app.use(cors() as RequestHandler);
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(express.static(path.join(__dirname, "../build")));
 app.use("/api", apiRouter);
--- code-editor-server/src/utils/constants.ts
+++ code-editor-server/src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const CONNECTION_STRING =
-  "yss://y-sweet-server-worker-staging.davidrd123.workers.dev";
+  "https://y-sweet-server-worker-staging.umbracapstoneteam.workers.dev";
