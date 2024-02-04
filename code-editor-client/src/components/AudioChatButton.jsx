cd4d1e044f03fd83f466c4bd188c2adb7a159282
Michael Johnson <elsif.maj@gmail.com>
2024-02-03 17:58:53 -0800

    code style tweaks

--- code-editor-client/src/components/AudioChatButton.jsx
+++ code-editor-client/src/components/AudioChatButton.jsx
@@ -75,26 +75,6 @@ const AudioChatButton = () => {
         />
       </Tooltip>
     )
-
-    // <Tooltip
-    //   label='Click to join audio chat'
-    //   fontSize='md'
-    //   bg={useColorModeValue("orange.200", "orange.900")}
-    //   color={useColorModeValue("gray.600", "white")}
-    // >
-    // <IconButton
-    //     icon={<PhoneIcon color='white' />}
-    //     ml={6}
-    //     onClick={handleAudioChatToggleButton}
-    //     variant='solid'
-    //     bg='linear-gradient(225deg, hsla(184, 100%, 54%, 1) 0%, hsla(210, 100%, 50%, 1) 100%)'
-    //     aria-label='Join Audio Chat'
-    //     _hover={{
-    //       bg: "linear-gradient(225deg, hsla(210, 100%, 50%, 1) 0%, hsla(184, 100%, 54%, 1) 100%)",
-    //     }}
-    //     textAlign='end'
-    //   />
-    // </Tooltip>
   )
 }
 
